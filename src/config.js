c89ae6e66205e2013bd13ecc8e582ff0d255f6ef
Vik <lifeasvik@gmail.com>
2020-05-11 09:53:57 -0500

    added changes to backend

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "bananas",
   JWT_EXPIRY: process.env.JWT_EXPIRY || 3000000,
 };
+// TODO
+// heroku create
+// add the db
+// npm run migrate:production
+// go to dashboard.herokuapp.com -> setup JWT_SECRET and JWT_EXPIRY as config vars
